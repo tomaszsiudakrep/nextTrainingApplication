7d557146dc29b8bcb20f75994f2ae62886efc70f
Tomasz Siudak <tomaszsiudak00@gmail.com>
2021-10-24 13:14:49 +0200

    Set step of reps in ladder

---
+++ src/main/java/app/data/SetRepLadderSettings.java
@@ -0,0 +1,22 @@
+package app.data;
+
+import app.scene.nextTraining.ladderScene.LadderSettingsScene;
+
+public class SetRepLadderSettings {
+
+    public static int defaultReps = 1;
+    public int intervalRep = 1;
+
+    public void plusIntervalRepToDefaultReps() {
+        defaultReps += intervalRep;
+        if (defaultReps == 1) LadderSettingsScene.repSettingsLabel.setText(defaultReps + " rep step");
+        else LadderSettingsScene.repSettingsLabel.setText(defaultReps + " reps step");
+    }
+
+    public void minusIntervalRepFromDefaultReps() {
+        defaultReps -= intervalRep;
+        if (defaultReps == 0) defaultReps = 1;
+        if (defaultReps == 1) LadderSettingsScene.repSettingsLabel.setText(defaultReps + " rep step");
+        else LadderSettingsScene.repSettingsLabel.setText(defaultReps + " reps step");
+    }
+}
